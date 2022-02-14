7f11c404870bb086be2cb55c769f3bb01d2e781a
Tomislav Markovica <tomislav.markovica1@gmail.com>
2022-02-14 19:47:13 +0100

    making design commit

--- raspoznavanje-bolesti-stitnjace/src/app/home/home.page.ts
+++ raspoznavanje-bolesti-stitnjace/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThyroidDiseaseRecord } from '../interfaces/thyroid-disease-record';
 import { ThyroidDiseaseRecordService } from '../services/thyroid-disease-record.service';
 
 @Component({
@@ -16,32 +17,63 @@ export class HomePage {
 
   age: number;
   gender: string[] = ["M","F"];
-  on_thyroxine: string[] = ["f","t"];
-  query_on_thyroxine: string[] = ["f","t"];
-  on_antithyroid_medication: string[] = ["f","t"];
-  sick: string[] = ["f","t"];
-  pregnant: string[] = ["f","t"];
-  thyroid_surgery: string[] = ["f","t"];
-  I131_treatment: string[] = ["f","t"];
-  query_hypothyroid: string[] = ["f","t"];
-  query_hyperthyroid: string[] = ["f","t"];
-  lithium: string[] = ["f","t"];
-  goitre: string[] = ["f","t"];
-  tumor: string[] = ["f","t"];
-  hypopituitary: string[] = ["f","t"];
-  psych: string[] = ["f","t"];
-  TSH_measured: string[] = ["f","t"];
+  on_thyroxine: boolean;
+  query_on_thyroxine: boolean;
+  on_antithyroid_medication: boolean;
+  sick: boolean;
+  pregnant: boolean;
+  thyroid_surgery: boolean;
+  I131_treatment: boolean;
+  query_hypothyroid: boolean;
+  query_hyperthyroid: boolean;
+  lithium: boolean;
+  goitre: boolean;
+  tumor: boolean;
+  hypopituitary: boolean;
+  psych: boolean;
+  TSH_measured: boolean;
   TSH: number;
-  T3_measured: string[] = ["f","t"];
+  T3_measured: boolean;
   T3: number;
-  TT4_measured: string[] = ["f","t"];
+  TT4_measured: boolean;
   TT4: number;
-  T4U_measured: string[] = ["f","t"];
+  T4U_measured: boolean;
   T4U: number;
-  FTI_measured: string[] = ["f","t"];
+  FTI_measured: boolean;
   FTI: number;
-  TBG_measured: string[] = ["f","t"];
+  TBG_measured: boolean;
   TBG: number;
   diagnosis: string;
 
+  thyroidDiseaseRecord: ThyroidDiseaseRecord = { 
+    age : 0,
+    gender :  "",
+    on_thyroxine :  false,
+    query_on_thyroxine :  false,
+    on_antithyroid_medication :  false,
+    sick :  false,
+    pregnant :  false,
+    thyroid_surgery :  false,
+    I131_treatment :  false,
+    query_hypothyroid :  false,
+    query_hyperthyroid :  false,
+    lithium :  false,
+    goitre :  false,
+    tumor :  false,
+    hypopituitary :  false,
+    psych :  false,
+    TSH_measured :  false,
+    TSH : 0,
+    T3_measured :  false,
+    T3 : 0,
+    TT4_measured :  false,
+    TT4 : 0,
+    T4U_measured :  false,
+    T4U : 0,
+    FTI_measured :  false,
+    FTI : 0,
+    TBG_measured :  false,
+    TBG : 0,
+    diagnosis :  ""
+  }
 }
